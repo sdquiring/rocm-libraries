7ae8627f54668fe66879799b6ab8e91522f334e9
Matthew Emmett <matthew.emmett@amd.com>
2025-12-22 15:49:14 -0700

    Add ScheduleMultiplyAndLDS transform.

--- shared/rocroller/lib/include/rocRoller/KernelGraph/Transforms/All.hpp
+++ shared/rocroller/lib/include/rocRoller/KernelGraph/Transforms/All.hpp
@@ -59,6 +59,7 @@
 #include <rocRoller/KernelGraph/Transforms/RemapOutputTiles.hpp>
 #include <rocRoller/KernelGraph/Transforms/RemoveDuplicates.hpp>
 #include <rocRoller/KernelGraph/Transforms/RemoveSetCoordinate.hpp>
+#include <rocRoller/KernelGraph/Transforms/ScheduleMultiplyAndLDS.hpp>
 #include <rocRoller/KernelGraph/Transforms/Simplify.hpp>
 #include <rocRoller/KernelGraph/Transforms/SwizzleScale.hpp>
 #include <rocRoller/KernelGraph/Transforms/UnrollLoops.hpp>
