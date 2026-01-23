9ba54f96a63f6b906607322ba85d1516f9020e7e
Scott Quiring <scott.quiring@amd.com>
2026-01-23 20:35:01 +0000

    Fix RemoveImplicitScheduling for non-MX cases.

--- shared/rocroller/test/catch/RemoveImplicitSchedulingTest.cpp
+++ shared/rocroller/test/catch/RemoveImplicitSchedulingTest.cpp
@@ -25,6 +25,7 @@
  *******************************************************************************/
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
 #include "TestContext.hpp"
@@ -61,81 +62,109 @@ namespace RemoveImplicitSchedulingTest
     {
         using namespace rocRoller::KernelGraph;
 
-        auto context = TestContext::ForDefaultTarget();
-
-        auto example = rocRollerTest::Graphs::GEMM(DataType::Float);
-
-        example.setTileSize(128, 128, 16);
-        example.setMFMA(32, 32, 2, 1);
-        example.setUseLDS(true, true, false);
-        example.setUnroll(2, 2);
-        example.setPrefetch(true, 2, 1, true);
-
-        auto graph  = example.getKernelGraph();
-        auto params = example.getCommandParameters();
-
-        // Apply full transform pipeline up to RemoveImplicitScheduling
-        graph = transform<IdentifyParallelDimensions>(graph);
-        graph = transform<OrderMemory>(graph, true);
-        graph = transform<UpdateParameters>(graph, params);
-        graph = transform<AddLDS>(graph, params, context.get());
-        graph = transform<LowerLinear>(graph, context.get());
-        graph = transform<LowerTile>(graph, params, context.get());
-        graph = transform<LowerTensorContraction>(graph, params, context.get());
-        graph = transform<Simplify>(graph);
-        graph = transform<ConstantPropagation>(graph);
-        graph = transform<FuseExpressions>(graph);
-        graph = transform<ConnectWorkgroups>(graph, context.get());
-        graph = transform<UnrollLoops>(graph, params, context.get());
-        graph = transform<FuseLoops>(graph);
-        graph = transform<RemoveDuplicates>(graph);
-        graph = transform<OrderEpilogueBlocks>(graph);
-        graph = transform<Simplify>(graph);
-        graph = transform<CleanLoops>(graph);
-        graph = transform<AddPrefetch>(graph, params, context.get());
-
-        auto multiplyNodesBefore
-            = graph.control.getNodes<ControlGraph::Multiply>().to<std::vector>();
-        REQUIRE(!multiplyNodesBefore.empty());
-
-        // Apply RemoveImplicitScheduling
-        auto transformedGraph = transform<RemoveImplicitScheduling>(graph);
-
-        auto colouring = rocRoller::KernelGraph::colourByUnrollValue(graph);
-
-        auto multiplyNodesAfter
-            = transformedGraph.control.getNodes<ControlGraph::Multiply>().to<std::vector>();
-
-        CHECK(multiplyNodesAfter == multiplyNodesBefore);
-
-        std::set<std::tuple<int, int>> orderedPairsBefore;
-        for(auto iter = multiplyNodesBefore.begin(); iter != multiplyNodesBefore.end(); ++iter)
+        auto dataTypeAB = GENERATE(DataType::Float, DataType::FP4);
+        DYNAMIC_SECTION("dataTypeAB=" << dataTypeAB)
         {
-            for(auto iter2 = std::next(iter); iter2 != multiplyNodesBefore.end(); ++iter2)
-            {
-                if(graph.control.compareNodes(UpdateCache, *iter, *iter2)
-                   != ControlGraph::NodeOrdering::Undefined)
-                {
-                    orderedPairsBefore.insert({*iter, *iter2});
-                }
-            }
-        }
 
-        int newlyUnorderedPairs = 0;
-        for(auto const& [node1, node2] : orderedPairsBefore)
-        {
-            auto order = transformedGraph.control.compareNodes(UpdateCache, node1, node2);
-            if(order == ControlGraph::NodeOrdering::Undefined)
+            auto dataTypeCD = dataTypeAB == DataType::Float ? DataType::Float : DataType::Half;
+            auto example
+                = rocRollerTest::Graphs::GEMM(dataTypeAB, dataTypeAB, dataTypeCD, dataTypeCD);
+
+            auto context = TestContext::ForDefaultTarget();
+
+            example.setUseLDS(true, true, false);
+            example.setUnroll(2, 2);
+            example.setPrefetch(true, 2, 1, true);
+
+            if(dataTypeAB == DataType::Float)
             {
-                CHECK(colouring.operationColour.at(node1) != colouring.operationColour.at(node2));
-                newlyUnorderedPairs++;
+                example.setMFMA(32, 32, 2, 1);
+                example.setTileSize(128, 128, 16);
             }
             else
             {
-                CHECK(order == graph.control.compareNodes(UpdateCache, node1, node2));
+                example.setTileSize(128, 128, 256);
+                example.setMFMA(16, 16, 128, 1);
+                example.setScaling(Operations::ScaleMode::Separate,
+                                   Operations::ScaleMode::Separate,
+                                   DataType::E8M0,
+                                   DataType::E8M0,
+                                   32);
+                example.setScaleLoadPaths(SolutionParams::LoadPath::BufferToLDS,
+                                          SolutionParams::LoadPath::BufferToLDS);
             }
+
+            auto graph  = example.getKernelGraph();
+            auto params = example.getCommandParameters();
+
+            // Apply full transform pipeline up to RemoveImplicitScheduling
+            graph = transform<IdentifyParallelDimensions>(graph);
+            graph = transform<OrderMemory>(graph, true);
+            graph = transform<UpdateParameters>(graph, params);
+            graph = transform<AddLDS>(graph, params, context.get());
+            graph = transform<LowerLinear>(graph, context.get());
+            graph = transform<LowerTile>(graph, params, context.get());
+            graph = transform<LowerTensorContraction>(graph, params, context.get());
+            graph = transform<Simplify>(graph);
+            graph = transform<ConstantPropagation>(graph);
+            graph = transform<FuseExpressions>(graph);
+            graph = transform<ConnectWorkgroups>(graph, context.get());
+            graph = transform<UnrollLoops>(graph, params, context.get());
+            graph = transform<FuseLoops>(graph);
+            graph = transform<RemoveDuplicates>(graph);
+            graph = transform<OrderEpilogueBlocks>(graph);
+            graph = transform<Simplify>(graph);
+            graph = transform<CleanLoops>(graph);
+            graph = transform<AddPrefetch>(graph, params, context.get());
+
+            auto multiplyNodesBefore
+                = graph.control.getNodes<ControlGraph::Multiply>().to<std::vector>();
+            REQUIRE(!multiplyNodesBefore.empty());
+
+            // Apply RemoveImplicitScheduling
+            auto transformedGraph = transform<RemoveImplicitScheduling>(graph);
+
+            REQUIRE(!multiplyNodesBefore.empty());
+
+            auto colouring = rocRoller::KernelGraph::colourByUnrollValue(graph);
+
+            REQUIRE(!multiplyNodesBefore.empty());
+
+            auto multiplyNodesAfter
+                = transformedGraph.control.getNodes<ControlGraph::Multiply>().to<std::vector>();
+
+            CHECK(multiplyNodesAfter == multiplyNodesBefore);
+
+            std::set<std::tuple<int, int>> orderedPairsBefore;
+            for(auto iter = multiplyNodesBefore.begin(); iter != multiplyNodesBefore.end(); ++iter)
+            {
+                for(auto iter2 = std::next(iter); iter2 != multiplyNodesBefore.end(); ++iter2)
+                {
+                    if(graph.control.compareNodes(UpdateCache, *iter, *iter2)
+                       != ControlGraph::NodeOrdering::Undefined)
+                    {
+                        orderedPairsBefore.insert({*iter, *iter2});
+                    }
+                }
+            }
+
+            int newlyUnorderedPairs = 0;
+            for(auto const& [node1, node2] : orderedPairsBefore)
+            {
+                auto order = transformedGraph.control.compareNodes(UpdateCache, node1, node2);
+                if(order == ControlGraph::NodeOrdering::Undefined)
+                {
+                    CHECK(colouring.operationColour.at(node1)
+                          != colouring.operationColour.at(node2));
+                    newlyUnorderedPairs++;
+                }
+                else
+                {
+                    CHECK(order == graph.control.compareNodes(UpdateCache, node1, node2));
+                }
+            }
+            CHECK(newlyUnorderedPairs > 0);
         }
-        CHECK(newlyUnorderedPairs > 0);
     }
 
 } // namespace RemoveImplicitSchedulingTest
