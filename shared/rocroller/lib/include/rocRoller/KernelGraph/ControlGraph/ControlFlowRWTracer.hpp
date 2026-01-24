f0893054c13b4e52c004737088d0013497583ca4
Scott Quiring <scott.quiring@amd.com>
2026-01-24 00:46:58 +0000

    Add test case for ControlFlowRWTracer::opReadWrite()

--- shared/rocroller/lib/include/rocRoller/KernelGraph/ControlGraph/ControlFlowRWTracer.hpp
+++ shared/rocroller/lib/include/rocRoller/KernelGraph/ControlGraph/ControlFlowRWTracer.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright 2024-2025 AMD ROCm(TM) Software
+ * Copyright 2024-2026 AMD ROCm(TM) Software
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -60,6 +60,8 @@ namespace rocRoller::KernelGraph
         {
             int       control, coordinate;
             ReadWrite rw;
+
+            auto operator<=>(ReadWriteRecord const& other) const = default;
         };
 
         ControlFlowRWTracer(KernelGraph const& graph,
